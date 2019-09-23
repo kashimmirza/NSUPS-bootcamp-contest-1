fb0c9d97cf966ce593149d375febef248ddc6e31
kashimmirza <45008789+kashimmirza@users.noreply.github.com>
2019-09-23 22:44:16 +0600

    perfectpermution

---
+++ perfectpermutation.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    int s=0;
+    cin>>n;
+    int v[n];
+   /* for(int i=0;i<n;i++){
+        v[i]=i+1;
+
+    }*/
+    if(n==1){
+
+
+    cout<<"-1"<<endl;
+    }
+    else{
+
+    for(int i=1;i<=n/2;i++ ){
+            cout<<2*i<<" "<<2*i-1<<" ";
+
+
+
+
+
+
+    }
+    }
+
+
+
+
+
+
+
+}
