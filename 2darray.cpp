48052410e082b23eded1701015582901d256c2b4
kashimmirza <45008789+kashimmirza@users.noreply.github.com>
2019-10-10 12:14:36 +0545

    2darray.cpp

---
+++ 2darray.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int s;
+    cin>>s;
+    int *row= new int[s];
+    //int *col=new int[s];
+
+  //char * *b=new *char[col];
+  for(int i=0;i<s;i++){
+         int *col=new int[s];
+    for(int j=0;j<s;j++){
+        cin>>col[j];
+
+
+    }
+
+  }
+
+
+
+}
